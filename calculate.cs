35a2c0baad2b708f973ebcc7148df2c8ef7dfa3b
ranma15g <ranma15g@gmail.com>
2024-06-29 17:00:17 +0000

    Comments Added

--- calculate.cs
+++ calculate.cs
@@ -1,8 +1,12 @@
+// Create the class calCulate to display messages for the user based on input
 public class calCulate
 {
 
+  // Create the method Welcome to display the menu
   public string Welcome()
   {  
+
+      // Create a number of strings to display the menu
       string w1 = ("************************************");
 
       string w12 = ("*  The stars shine in Greenville.  *");
@@ -15,29 +19,37 @@ public class calCulate
 
       string w5 = ("2. Exit");
 
-      string wFinal = w1 + "\n" + w12 + "\n" + w2 + "\n\n" + w3 + "\n\n" + w4 + "\n" + w5;
+      // Return the strings representing the menu
+      return "\n" + w12 + "\n" + w2 + "\n\n" + w3 + "\n\n" + w4 + "\n" + w5;
 
-      return wFinal;
   }
 
 
+
+  
+    // Create the method GetMessage to display the message for option 1
   public string GetMessage()
   {
+    // Create a set of strings to display the message
     string s1 = "Last year's competition had 122 contestants, and this year's has 426 contestants\n";
 
     string s2 = "Revenue expected this year is $10,650\n";
 
     string s3 = "It is true that this year's competition is bigger than last year's.\n";
 
+    // Return the strings representing the message when the user selects input 1
     return s1 + s2 + s3;
   }
 
 
+  
+  // Create the method Ciao to display the good bye message
   public string Ciao()
   {
-    string e1 = "Thank you for using the Greenville Revenue App, good-bye!";
 
-    return e1;
+    // Return the good bye message for the user
+    return "Thank you for using the Greenville Revenue App, good-bye!";
+
   }
 
 
--- main.cs
+++ main.cs
@@ -2,35 +2,46 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-
+    // Instantiate an object cobj of the class calCulate
     calCulate cobj = new calCulate();
 
+    // Create an infinite loop to display the menu until user exits
     while(true)
 
     {
+      // Access the Welcome method from the calCulate class in the cobj object to return a value for the menu
       var intro = cobj.Welcome();
 
+      // Display the menu
       Console.WriteLine(intro);
 
+      // Receive the user's input
       var choice = Console.ReadLine();
 
 
-
+      // If the user chooses option 1
       if (choice == "1") 
       {
+        // Access the GetMessage method in the cobj object to return a calculation message
         var answer = cobj.GetMessage();
 
+        // we display the message
         Console.WriteLine("\n" + answer);
       }
 
 
-
+      // if the user chooses option 2
       if (choice == "2")
       {
+        // Access the Ciao method in the cobj object to return a good bye message 
+        to be displayed
+
         var goodBi = cobj.Ciao();
 
+        // display the good bye message
         Console.WriteLine("\n" + goodBi);
 
+        // exit the program
         break;
       }
 
